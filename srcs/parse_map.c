71e8fa1370d4c66c8ca2190256207fb7ec020bee
eisikogl <isikoglu99@gmail.com>
2022-12-30 14:57:01 +0300

    removed setting file

--- srcs/parse_map.c
+++ srcs/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: eisikogl <eisikogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 14:55:50 by eisikogl          #+#    #+#             */
-/*   Updated: 2022/12/29 17:01:06 by eisikogl         ###   ########.fr       */
+/*   Updated: 2022/12/30 14:53:32 by eisikogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ int	ft_check_closed(t_gamedata *cub)
 		{
 			if ((i == 0 || j == 0 || i == cub->map_h - 1 || j == cub->map_w - 1)
 				&& !(cub->map[i][j] == '1' || cub->map[i][j] == ' '))
-				return (ft_error("Map not closed : |%s|\n"));
+				return (ft_error("Map not closed"));
 			if (cub->map[i][j] == ' ')
 			{
 				if (!ft_check_y_axis(cub, i, j) || !ft_check_x_axis(cub, i, j))
